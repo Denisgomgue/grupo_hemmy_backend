6cf5289d2f9c156648fc5e8aad1447bbdf022f74
Denisgomgue <gomezggdenis@gmail.com>
2025-06-11 18:38:48 -0500

    bat-none

---
+++ npm_startup.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Iniciando npm run start:dev...
+cd /d "D:\group_hemmy\grupo_hemmy_backend>"
+start cmd /k npm run start:dev
+echo Iniciando npm run dev...
+cd /d "D:\group_hemmy\grupo_hemmy_frontend>"
+start cmd /k npm run dev
+exit

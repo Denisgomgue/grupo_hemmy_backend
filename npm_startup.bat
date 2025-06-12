41af8f2981a8b67a69f8650f1705f063bf82049c
Denisgomgue <gomezggdenis@gmail.com>
2025-06-11 20:20:58 -0500

    bat nones

--- npm_startup.bat
+++ npm_startup.bat
@@ -1,16 +1,17 @@
-
 @echo off
 echo Iniciando npm run start:dev...
-cd /d "D:\group_hemmy\grupo_hemmy_backend>"
+cd /d "D:\group_hemmy\grupo_hemmy_backend"
 start cmd /k npm run start:dev
-echo Esperando a que el puerto 3001 este ocupado...
+
+echo Esperando a que el puerto 3000 este ocupado...
 :CHECK_PORT
 timeout /t 2 /nobreak >nul
-netstat -aon | findstr ":3001" | findstr "LISTENING" >nul
+netstat -aon | findstr ":3000.*LISTENING" >nul
 if errorlevel 1 (
     goto CHECK_PORT
 )
-echo Puerto 3001 ocupado, iniciando npm run dev...
+
+echo Puerto 3000 ocupado. Iniciando npm run dev...
 cd /d "D:\group_hemmy\grupo_hemmy_frontend"
 start cmd /k npm run dev
 exit
